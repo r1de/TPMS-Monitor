018f4273b08e5dd1ed9e4242e2cf945f0f5a5c61
Eric <kgwxc@gmail.com>
2025-09-16 18:59:52 -0700

    fix stupid mistake

--- TPMS-Monitor.py
+++ TPMS-Monitor.py
@@ -22,18 +22,9 @@ from bleak.backends.device import BLEDevice
 from bleak.backends.scanner import AdvertisementData
 
 #Get the sensor addresses from the other file
-import sensorMacAddresses
+from sensorMacAddresses import sensors
 
 #import pprint
-
-class sensors:
-	########################################
-	# Update the sensor MAC addresses here #
-	########################################
-	FL_SENSOR = "4B:A8:00:00:4F:43" #Front Left
-	FR_SENSOR = "4B:9D:00:00:4A:2A" #Front Right
-	RL_SENSOR = "11:22:33:44:55:66" #Rear Left
-	RR_SENSOR = "AA:BB:CC:DD:EE:FF" #Rear Right
 
 #Parse the data RX'd from the sensors
 def parse_sensor_data(advertisement_data):
