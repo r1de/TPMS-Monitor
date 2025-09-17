47bfcdbdaa4718016d78c86626a5ff0da573f02e
Eric <kgwxc@gmail.com>
2025-09-16 19:01:38 -0700

    re-do the example mac addresses in sensorMacAddresses

---
+++ sensorMacAddresses.py
@@ -0,0 +1,12 @@
+class sensors:
+	########################################
+	# Update the sensor MAC addresses here #
+	# Only edit the MAC addresses          #
+	# Do Not change anything else          #
+	########################################
+	FL_SENSOR = "AA:BB:CC:DD:EE:FF" #Front Left
+	FR_SENSOR = "FF:AA:BB:CC:DD:EE" #Front Right
+	RL_SENSOR = "EE:FF:AA:BB:CC:DD" #Rear Left
+	RR_SENSOR = "DD:EE:FF:AA:BB:CC" #Rear Right
+	X1_SENSOR = "CC:DD:EE:FF:AA:BB" #Front Left
+	X2_SENSOR = "BB:CC:DD:EE:FF:AA" #Front Right
